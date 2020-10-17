2134389fec4e1bd9915233d1b80cbf6a5ed25ebb
Glitch (sunucubotu) <none>
2020-10-17 08:55:46 +0000

    🐠🚕 Checkpoint ./komutlar/abone.js:23205691/292

--- komutlar/abone.js
+++ komutlar/abone.js
@@ -1,13 +1,17 @@
 const Discord = require('discord.js');
 
 exports.run = function(client, message, args) {
-  let abone = message.mentions.members.first()
-  if(!message.member.hasPermission('ADMINISTRATOR')) return
+  let kullanıcı = message.mentions.members.first()
+  if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Bu komutu sadece yetkililer kullanabilir').then(mesaj => mesaj.delete(3000))  
   var role = message.guild.roles.find(role => role.id === "761223163988017173"); 
-  abone.addRole(role);
+  kullanıcı.addRole(role);
   let embed = new Discord.RichEmbed()
   .setTitle(` <a:jke:751558669585612830> • __\` Abone Rolü Başarıyla Verildi \`__   `)
-  .setDescription(`<a:jke:751558669585612830> • __**\` Yetkili \`**__ ${message.author}`)
+  .setDescription(`
+<a:jke:751558669585612830> • __**\` Yetkili \`**__ ${message.author}
+
+<a:jke:751558669585612830> • __**\` Kullanıcı \`**__ ${kullanıcı}`)
+  
   message.channel.send(embed);
 };
 
