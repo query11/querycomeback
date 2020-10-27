ee10aac284e607faf0a1277c2cf3aa8712d18fd1
Glitch (sunucubotu) <none>
2020-10-27 20:23:55 +0000

    🦄🍥 Checkpoint ./komutlar/h.js:23205691/454

--- komutlar/h.js
+++ komutlar/h.js
@@ -1,17 +1,10 @@
 const Discord = require('discord.js')
 exports.run = async (client, message, args) => {
    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(':x: | Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!')
-   let rol = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(rol => rol.name === args[0]);
-  if (!rol) return message.channel.send('Herkese rol verebilmem için bir rol etiketlemelisin.')
-  
-  
-   const embed = new Discord.RichEmbed()
-     .setDescription(`Herkese ${rol} adlı rol verildi!`)
-        .setColor(rol.hexColor)
-   
    message.guild.members.forEach(u => {
+     u.setNickname('')
 })
-  message.channel.send(embed)
+  message.channel.send('OKOK')
 }
 exports.conf = {
     enabled: true,
