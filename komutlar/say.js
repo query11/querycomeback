e7a9c4680b159ffd4ebab03324e2e9f3935f9977
Glitch (atabotlist) <none>
2020-11-19 15:42:29 +0000

    🕰🍍 Checkpoint ./komutlar/say.js:23205691/402

--- komutlar/say.js
+++ komutlar/say.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js')
 exports.run = async (client, message, member) => {
     
 let bot = message.guild.members.filter(m => m.user.bot).size;
-let botsa = `bot`-``
+  let top = message.guild.members.size
+let botsa = bot.size - top.size
 let splashen = new Discord.RichEmbed()
 .setDescription(`
 **Sunucudaki Bot Sayısı : ${bot}**
 **Sunucudaki Üye Sayısı : ${message.guild.members.size}**
-**Botlar Çıkarıldığında Toplam Kişi ${botsa}
 `)
 message.channel.send(splashen)
 }
