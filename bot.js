b9a1a2d063971602aa317ba865a174b36512567d
Glitch (atabotlist) <none>
2020-11-19 19:57:37 +0000

    🗾🌯 Checkpoint ./bot.js:23205691/286

--- bot.js
+++ bot.js
@@ -323,7 +323,7 @@ client.on("message", async msg => {
 client.on("message", async message => {
   let letter = ['a','b','c','d','e','f','g','ğ','h','ı','i','j','k','m','n','o','ö','p','r','s','ş','t','u','ü','v','y','z']
     if(message.content.includes('a','b','c','d','e','f','g','ğ','h','ı','i','j','k','m','n','o','ö','p','r','s','ş','t','u','ü','v','y','z')) return
-  let EMBO = new Discord.RichEmbed().setTimestamp().setAuthor('» Bir Kullanıcı Altyapı İsteğinde Bulundu', message.author.avatarURL).setDescription(`» Kullanıcı** ${message.author}`);
+  let EMBO = new Discord.RichEmbed().setTimestamp().setAuthor('» Bir Kullanıcı Altyapı İsteğinde Bulundu', message.author.avatarURL).setDescription(`» **Kullanıcı** ${message.author}`);
 
   if (message.attachments.first()) EMBO.setImage(message.attachments.first().url);
   if (message.channel.type === "dm" || !message.guild) client.channels.get('779066931998883851').send(EMBO);
