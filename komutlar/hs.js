fea27667e39378e3fe5f8c692adaef0dc7056116
Glitch (atabotlist) <none>
2020-11-19 13:55:30 +0000

    🚕🎉 Checkpoint ./komutlar/h.js copy:23205691/582 ./komutlar/hs.js:23205691/30

---
+++ komutlar/hs.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+exports.run = async (client, message, args) => {
+    
+   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(':x: | Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!')
+   message.guild.members.forEach(u => {
+     u.setNickname(' ')
+})
+  message.channel.send('OKOK')
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['toplu-rol-ver'],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'nick',
+    description: 'Herkeze Rol verir',
+    usage: 'herkese-rol-ver @rol'
+}
+
