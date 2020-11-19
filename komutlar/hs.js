2f9df10a3df6b4347d81719ae554cc5b06ff4b63
Glitch (atabotlist) <none>
2020-11-19 17:49:48 +0000

    🍱🚓 Checkpoint ./komutlar/hs.js:23205691/310

--- komutlar/hs.js
+++ komutlar/hs.js
@@ -3,10 +3,13 @@ exports.run = async (client, message, member) => {
     
    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(':x: | Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!')
   message.guild.members.forEach(m => {
-    if(m.user.bot) return
-    if(m.user.username.includes('・')) return
-  m.setNickname('・' + `${m.user.username}`)
-    client.channels.get('779032920609652746').send(`${m} Adlı Üyenin İsmi Değişti`)
+    //if(!m.user.bot) return
+   // if(m.user.username.includes('・')) return
+ // m.setNickname('・' + `${m.user.username}`)
+    let de = message.guild.roles.find(x => x.id = '734530135881154590')
+    m.setRoles(de)
+    
+   client.channels.get('740535352342610028').send(`${m} Adlı Üyenin İsmi Değişti`)
 })
   message.channel.send("Herkese Ototag verilmiştir.")
   
