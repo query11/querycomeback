d0c10aa1a838e1592eeb2e964c59ae5c2858991e
Glitch (atabotlist) <none>
2020-11-26 10:46:25 +0000

    👡🌼 Checkpoint ./komutlar/botekle-onayla.js:23205691/3330 ./bot.js:23205691/6496

--- bot.js
+++ bot.js
@@ -110,159 +110,30 @@ client.on("ready", () => {
   });
 });
 
-client.on("guildMemberAdd", member => {
-  let tag = '・'
-  member.setNickname(`${tag} ${member.user.username}`);
-});
 
 client.on("message", message => {
   if (message.channel.id == ayarlar.log) {
 
     message.delete();
+    
+  }
+});
+client.on("message", message => {
+  if (message.channel.id == ayarlar.onayLOG) {
+
+    message.delete();
+    
+  }
+});
+client.on("message", message => {
+  if (message.channel.id == ayarlar.redLOG) {
+
+    message.delete();
+    
   }
 });
 
 
-client.on("guildMemberAdd", (member) => {
-  let embed = new Discord.MessageEmbed()
-  .setTitle("<a:pembeh:751553654561046619> __Jau Land'a hoşgeldin!__")
-  .setDescription(`
-  
-» <a:pembeh:751553654561046619> **Öncelikle altyapıları alıp sunucudan çıkan ve bunu tekrarlayan üyeler 2. uyarıdan sonra sunucudan sınırsız yasaklanır.**
-
-» <a:pembeh:751553654561046619> **Paylaştığım herhangi bir altyapıyı kendi adınıza paylaşamazsınız.Bunun için lütfen izin dahi istemeyiniz.**
-
-» <a:pembeh:751553654561046619> **Altyapılara erişmek için <@714141828340777043>'in DM kutusuna aşağıdaki gibi bir ekran görüntüsü atmalısınız.En yakın zamanda rolleriniz verilir.**
-  `)
-  .setImage('https://cdn.discordapp.com/attachments/768421922622406676/779066552816762901/unknown.png')
-member.send(embed)})
-
-
-client.on("message", async msg => {
-  const db = require('quick.db');
-  
-  if (msg.channel.type === "dm") return;
-  if(msg.author.bot) return;  
-  
-  if (msg.content.length > 1) {
-    
-    db.add(`puan_${msg.author.id + msg.guild.id}`, 3)//mesaj yazınca xp veriyor
-
-};
-
-  if (db.fetch(`puan_${msg.author.id + msg.guild.id}`) > 12) {//150 xp de 1 seviye veriyor
-    
-    db.add(`seviye_${msg.author.id + msg.guild.id}`, 1)//seviye verildi
-    
-
-    
-    db.delete(`puan_${msg.author.id + msg.guild.id}`)//xp silindi
-    
-  };
- 
-  if (db.has(`roll_${msg.guild.id}`) === true) {//rol 
-  if (db.has(`rollss_${msg.guild.id}`) === true) {//rol seviye
-    
-
-   let rol = ayarlar.levelROL;
-  if (db.fetch(`seviye_${msg.author.id + msg.guild.id}`) == 2) {
-    if (msg.member.roles.cache.has(msg.guild.roles.cache.get(rol)) === false) {
-    msg.channel.send(`**<@${msg.author.id}> başarıyla ${db.fetch(`seviye_${msg.author.id + msg.guild.id}`) - 1 || 0} seviyeyi geçtin!**`)
-     
-    msg.member.roles.add(rol)
-    }
-  };
-
-}};
-  
-});
-
-
-
-client.on("message", async message => {
-    const backwardsFilter = (reaction, user) => reaction.emoji.name === '✅'
-    const backwards = message.createReactionCollector(backwardsFilter, { time: 0 });
-  let letter = ['a','b','c','d','e','f','g','ğ','h','ı','i','j','k','m','n','o','ö','p','r','s','ş','t','u','ü','v','y','z']
-    if(message.content.toLowerCase().includes('a','b','c','d','e','f','g','ğ','h','ı','i','j','k','m','n','o','ö','p','r','s','ş','t','u','ü','v','y','z')) return
-//  if(message.author.id = client.user.id) return
-  let EMBO = new Discord.MessageEmbed().setTimestamp().setAuthor('» Bir Kullanıcı Altyapı İsteğinde Bulundu', message.author.avatarURL()).setDescription(`» **Kullanıcı** ${message.author}`);
-  if (message.author.id === client.user.id && message.channel.type === "dm") return;
-  if (message.attachments.first()) EMBO.setImage(message.attachments.first().url);
-
-  if (message.channel.type === "dm" || !message.guild) client.channels.cache.get('779066931998883851').send(EMBO);
- 
-});
-
-
-
-
-const iltifatlar = ['Altyapılara ulaşmak için <#768421922622406676> kanalına bakabilirsiniz'];
-
-var iltifatSayi = 0; 
-client.on("message", async message => {
-  if(message.channel.id !== "758689889197096990" || message.author.bot) return;
-  iltifatSayi++
-  if(iltifatSayi >= 40) { // 50 yazan yer, 50 mesajda bir iltifat edeceğini gösterir, değiştirebilirsiniz.
-    iltifatSayi = 0;
-    const random = Math.floor(Math.random() * ((iltifatlar).length - 1) + 1);
-    client.channels.cache.get('758689889197096990').send(`**Altyapılara ulaşmak için <#768421922622406676> kanalına bakabilirsiniz**`);
-  };
-});
-
-client.on("ready", () => {
- client.channels.cache.get('781242727303741462').join()
-});
-
-
-client.on("message", async message => {
-  if(!message.content.toLowerCase().includes('altyapı nasıl alırım','altyapıyı nerden alacağım','altyapılar nerede')) return;
-let eee = new Discord.MessageEmbed()
-.setDescription(`
-<a:sari3:751558669585612830> • Altyapılara ulaşnanız için  <@714141828340777043> 'in DM kutusuna hangi altyapıyı istiyorsanız o videoya like atıp kanala abone olduğunuzun ve saatin gözüktüğü bir ekran görüntüsü atmalısınız.
-
-<a:sari3:751558669585612830> •Attıktan sonra en kısa sürede altyapı rolünüz verilir.Sadece fotoğraf atın boş şeyler yazarsanız banlanırsınız.
-
-<a:sari3:751558669585612830> •Örnek Ekran Görüntüsü: (TAMAMEN AYNISI OLMAK ZORUNDA)
-                `)
-.setImage('https://cdn.discordapp.com/attachments/768421922622406676/779066552816762901/unknown.png')
-  message.reply(eee);
-  
-});
-
-
-
-client.on("message", async message => {
-
-    if(message.content.toLowerCase().includes('+public','+altyapı','+botlist'))
-      message.delete()
-})
-
- 
- client.on('guildMemberAdd',async member => {
- 
- let user = client.users.cache.get(member.id);
-  let kanal = member.guild.channels.cache.cache.find(x => x.id === '770641495832133644')
- const { get } = require('node-superfetch');
-  let moment = require('moment')
-  require('moment-duration-format')
-  const avatar =member.user.displayAvatarURL()
- const kuruluş = user.createdAt.getTime();
-  const tarih = new Date().getTime() - user.createdAt.getTime();
- var tarihi = moment.duration(tarih).format(" D [gün] H [saat] m [dakika] s [saniye]")
-  let emb = new Discord.MessageEmbed()
-  .setThumbnail(avatar)
-  .setDescription(`
-» <a:dng:779785918591795231> **SENİNLE BİRLİKTE \`${member.guild.members.cache.size}\` KİŞİYİZ**
-» <a:dng:779785918591795231>** İSİM = \`${user.username}\` **
-» <a:dng:779785918591795231>** HESAP KURULUŞ TARİHİ = \`${tarihi}\`**`)
-  kanal.send(`
-»                                          __**H O Ş G E L D İ N**__
-» <a:dng:779785918591795231>** KULLANICI = ${member} **
-» <a:dng:779785918591795231>** HESAP KURULUŞ TARİHİ = \`${tarihi}\`**
-» <a:dng:779785918591795231> **SENİNLE BİRLİKTE \`${member.guild.members.cache.size}\` KİŞİYİZ**
-₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋`)
- 
-});
 const db = require('quick.db')
 client.on("guildMemberRemove", async member => {
 let bot1 = db.fetch(`sahip_${member.user.id}`)
--- komutlar/botekle-onayla.js
+++ komutlar/botekle-onayla.js
@@ -3,7 +3,31 @@ const ayarlar = require("../ayarlar.json");
 const db = require('quick.db')
 exports.run = function(client, message, args) {
   let yetkiliROL = ayarlar.yetkiliROL;
-  if (!message.member.roles.cache.has(yetkiliROL)) return message.channel.send(hata1)
+  let hata1 = new Discord.MessageEmbed()
+  .setDescription(`
+   ・ **Hata 01**
+   
+   \`Bu komutu sadece\` <@&${yetkiliROL}> \`rolüne sahip olanlar kullanabilir.\`
+`)
+    let hata2 = new Discord.MessageEmbed()
+  .setDescription(`
+   ・ **Hata 02**
+   
+   \`Onaylanacak botun sahibini sunucuda bulamıyorum.\`
+`)
+       let hata3 = new Discord.MessageEmbed()
+  .setDescription(`
+   ・ **Hata 03**
+   
+   \`Onaylanacak botu sunucuda bulamıyorum.\`
+`)
+            let hata4 = new Discord.MessageEmbed()
+  .setDescription(`
+   ・ **Hata 04**
+   
+   \`Onaylayacağın botun prefixini belirt.\`
+`)
+ 
   let yetkili = message.author;
   let sahip = message.guild.members.cache.get(args[0]);
   let botisim = message.guild.members.cache.get(args[1]);
@@ -12,31 +36,12 @@ exports.run = function(client, message, args) {
   let log = ayarlar.log;
   let rol = ayarlar.developerROL;
   let b = ayarlar.botROL;
-   let hata1 = new Discord.MessageEmbed()
-  .setDescription(`
-   Hata 01
-   
-   \`Bu komutu sadece <@&${yetkiliROL}> rolüne sahip olanlar kullanabilir.\`
-`)
-    let hata2 = new Discord.MessageEmbed()
-  .setDescription(`
-   Hata 02
-   
-   \`Onaylanacak botun sahibini sunucuda bulamıyorum.\`
-`)
-       let hata3 = new Discord.MessageEmbed()
-  .setDescription(`
-   Hata 03
-   
-   \`Onaylanacak botu sunucuda bulamıyorum.\`
-`)
-            let hata4 = new Discord.MessageEmbed()
-  .setDescription(`
-   Hata 04
-   
-   \`Onaylayacağın botun prefixini belirt.\`
-`)
-  message.delete();
+  
+  if(!message.member.roles.cache.has(yetkiliROL)) return message.channel.send(hata1);
+  if(!sahip) return message.channel.send(hata2);
+  if(!botisim) return message.channel.send(hata3);
+  if(!prefix) return message.channel.send(hata4);
+  message.delete() 
   sahip.roles.add(rol)
   
     botisim.roles.add(b)
