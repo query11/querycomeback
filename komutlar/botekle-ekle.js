77bd1f05a7bdc6e7f3279881b4adc45ba1cd138c
Glitch (atabotlist) <none>
2020-11-26 11:43:59 +0000

    🍘👁 Checkpoint ./komutlar/botekle-ekle.js:23205691/3463

--- komutlar/botekle-ekle.js
+++ komutlar/botekle-ekle.js
@@ -18,13 +18,17 @@ let botID = args[0];
       .send(`Bu komutu sadece <#${eklekanal}> kanalında kullanabilirsin.`)
       .then(msg => msg.delete({timeout : '3000'}));
   if (message.channel.id == eklekanal) {
-    if (!botID)
-      return 
-    if (!prefix)
-      return 
-    message.delete();
 
-    let embed2 = new Discord.MessageEmbed()
+
+    let emba = new Discord.MessageEmbed()
+     .setColor('#fff76b')
+    .setDescription(`
+    <:tr:780484679227932704>**Botunuz başarıyla sıraya eklendi,en yakın zamanda test edilecektir** 
+    <:en:780485586535448616>**Your bot has been successfully queued,will be checked as soon as possible** 
+    ₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋
+   **<:tr:780484679227932704> » Sıra | <:en:780485586535448616> Queue  [⤙ ${sıra} ⤚](https://www.youtube.com/channel/UCDf5rQLAZOfi6NV7on2S_AA/featured)**  `)
+    message.author.send(emba)
+        let embed2 = new Discord.MessageEmbed()
     .setColor('#fff76b')
     .setDescription(`
     <:tr:780484679227932704>**Bir bot başvurusu gönderildi** | <:en:780485586535448616>**A bot application has been submitted** 
@@ -41,15 +45,7 @@ let botID = args[0];
 
   **₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋ [0 Perm Ekle](https://discordapp.com/oauth2/authorize?client_id=${botID}&scope=bot&permissions=0) | ` + ` [8 Perm Ekle](https://discordapp.com/oauth2/authorize?client_id=${botID}&scope=bot&permissions=8) ₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋**`)
     
-    client.channels.cache.get(log).send(embed2);
-    let emba = new Discord.MessageEmbed()
-     .setColor('#fff76b')
-    .setDescription(`
-    <:tr:780484679227932704>**Botunuz başarıyla sıraya eklendi,en yakın zamanda test edilecektir** 
-    <:en:780485586535448616>**Your bot has been successfully queued,will be checked as soon as possible** 
-    ₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋
-   **<:tr:780484679227932704> » Sıra | <:en:780485586535448616> Queue  [⤙ ${sıra} ⤚](https://www.youtube.com/channel/UCDf5rQLAZOfi6NV7on2S_AA/featured)**  `)
-    message.author.send(emba)
+    client.channels.cache.get(log).send(embed2)
     db.set(`sahip_${message.author.id}`, botID)
     db.add(`sıra_${message.guild.id}`,1)
     db.add(`botsayi_${message.author.id}`,1)
