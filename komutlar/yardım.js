d1e21225a00e211108115b102e416144df152ae2
Glitch (atabotlist) <none>
2020-11-27 14:31:57 +0000

    ☺️😂 Checkpoint ./komutlar/yardım.js:23205691/1086 ./komutlar/kedi.js:23205691/755 ./komutlar/köpek.js:23205691/725 ./package.json:23205691/2051

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,13 +5,19 @@ exports.run = (client, message, args) => {
   if (message.channel.type !== "text") return;
   const yardım = new Discord.MessageEmbed()
   .setDescription(`
-  **Moderasyon Komutları**
-  \`-ban\` | \`-unban\` | \`-kick\` |  \`-kilit\` |  \`-kilitaç\` | \`-uyar\` | 
+    :white_small_square: **Jau Bot Yardım Komutları**
   
-  **Kullanıcı Komutları**
-   \`-avatar\` |  \`-yardım\` |  \`-istatistik\` |  \`-hakkında\` | 
+  __**Moderasyon Komutları**__
+  ・\`-ban\` | \`-unban\` | \`-kick\` |  \`-kilit\` |  \`-kilitaç\` | \`-uyar\` | 
+  
+  __**Kullanıcı Komutları**__
+  ・\`-avatar\` |  \`-yardım\` |  \`-istatistik\` |  \`-hakkında\` | 
+  
+  __**Eğlence Komutları**__
+  ・\`-köpek\` |  \`-\` |  \`-\` |  \`-\` | 
   
   `)
+  .setImage('https://cdn.discordapp.com/attachments/620989964104237077/781817709653393458/ezgif-4-76d567677887.gif')
   message.channel.send(yardım)
 };
 exports.conf = {
