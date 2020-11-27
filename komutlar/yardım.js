e0dddf3b53b07ce27cbebd479563b26323f1e741
Glitch (atabotlist) <none>
2020-11-27 09:25:16 +0000

    🕴⏰ Checkpoint ./komutlar/yardım.js:23205691/453 ./komutlar/covid.js:23205691/4485 ./package.json:23205691/3855 ./komutlar/istatistik.js:23205691/8129

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
  
 exports.run = (client, message, args) => {
-  
   const yardım = new Discord.MessageEmbed()
   .setDescription(`
   **Moderasyon Komutları**
   \`-ban\` | \`-unban\` | \`-kick\` |  \`-kilit\` |  \`-kilitaç\` 
   
-  **
-  
-  
+  **Kullanıcı Komutları**
+   \`-avatar\` |  \`-yardım\` |  \`-istatistik\` |  \`-ping\` |  \`-hakkında\` | 
   
   `)
   message.channel.send(yardım)
@@ -22,5 +20,6 @@ exports.conf = {
 };
  
 exports.help = {
-  name: 'yardım'
+  name: 'yardım',
+  açıklama : 'Botun yardım komutlarını gösterir.'
 };//
