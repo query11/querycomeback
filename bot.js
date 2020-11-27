3a94a77297f12af29d236048062e93c9eedc852f
Glitch (atabotlist) <none>
2020-11-27 07:14:28 +0000

    🐿☂️ Checkpoint ./komutlar/purge.js:23205691/2201 ./komutlar/kilit.js:23205691/1881 ./komutlar/unlock.js:23205691/845 ./bot.js:23205691/671

--- bot.js
+++ bot.js
@@ -109,22 +109,3 @@ client.on("ready", () => {
     status: "online"
   });
 });
-const db= require('quick.db')
-const ms = require('ms')
-client.on('guildMemberAdd', async(member) => {
- let mute = member.guild.roles.find(r => r.name === "Susturuldu");
-let mutelimi = db.fetch(`muteli_${member.guild.id + member.id}`)
-let süre = db.fetch(`süre_${member.id + member.guild.id}`)
-if (!mutelimi) return;
-if (mutelimi == "muteli") {
-member.addRole(mute.id)
- 
-member.send("Muteliyken Sunucudan Çıktığın için Yeniden Mutelendin!")
- setTimeout(function(){
-    // msg.channel.send(`<@${user.id}> Muten açıldı.`)
-db.delete(`muteli_${member.guild.id + member.id}`)
-    member.send(`<@${member.id}> Muten açıldı.`)
-    member.removeRole(mute.id);
-  }, ms(süre));
-}
-})
