f65e501caba7ab697d60f865c16d6bc3af642274
Glitch (atabotlist) <none>
2020-11-28 22:48:53 +0000

    🚖⚡️ Checkpoint ./komutlar/cmm.js:23205691/2691

--- komutlar/cmm.js
+++ komutlar/cmm.js
@@ -1,39 +1,23 @@
-const Discord = require("discord.js");
-const { get } = require("superagent");
-module.exports.run = async (bot, message, args) => {
-    try {
-        if(!args[0]){
-          let üyeHATA = new Discord.MessageEmbed()
-  .setDescription(`
-  Yanlış komut kullanımı \`-clyde [mesaj]\``)
-   .setFooter('Mesajınızda türkçe karakter bulunmadığından emin olun.')
-message.channel.send(üyeHATA)
-return;
-}
-        let url = `https://nekobot.xyz/api/imagegen?type=changemymind&text=${args.join(" ")}`
-        get(url).then(res => {
-            const embed = new Discord.MessageEmbed()
-            .setColor("fad0dd")
-            .setAuthor("")
-            .setImage(res.body.message)
-            setTimeout(() => {
-                return message.channel.send(embed);
-            }, 100);
-        });
-    } catch(err) {
-        console.log(err)    
-    }
-}
-module.exports.conf = {
+const Discord = require('discord.js');
+
+exports.run = async(client, message) => {
+  var espri = Math.floor(Math.random() * 100)
+  message.channel.send(``)
+    
+ 
+};
+
+exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["cmm"],
-  permLevel: 0
+  aliases: [],
+  permLevel: 0,
 };
- 
-module.exports.help = {
-  name: 'cmm',
-  category: 'Kullanıcı',
-  description: 'Change My Mind ',
-  usage: 'cmm <yazı>'
+
+exports.help = {
+  name: 'kaçcm',
+  description: 'Malafatının Büyüklüğünü Söyler.',
+  usage: 'kaçcm',
+  kategori: 'eğlence'
 };
+
