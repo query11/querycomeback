f1594d33482da80f86d3bbb9b3ddcb9340f729f9
Glitch (atabotlist) <none>
2020-11-28 16:37:45 +0000

    🍴⛪️ Checkpoint ./komutlar/mute.js:23205691/861

--- komutlar/mute.js
+++ komutlar/mute.js
@@ -4,8 +4,11 @@ const ms = require('ms');
 
 exports.run = async (client, message, args) => {// can#0002
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
-
-if(!args[0]) return message.channel.send(``);
+    let üyeHATA2 = new Discord.MessageEmbed()
+  .setDescription(`
+  Yanlış komut kullanımı \`-mute [@kullanıcı] [süre] [sebep]\`
+  ・Süreler \`1s = 1 Saniye | 1m = 1 Dakika | 1h = 1 Saat\``)
+if(!args[0]) return message.channel.send(üyeHATA2);
 
 let member = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || message.guild.members.cache.find(a => message.guild.members.cache.get(a.user.id).nickname && a.nickname.toLowerCase().includes(args[0].toLowerCase())) || message.guild.members.cache.find(a => a.user.username.toLowerCase().includes(args[0].toLowerCase()))
 if(!member) return message.channel.send(`Member "${args[0]}" not found`);
@@ -56,12 +59,12 @@ return message.channel.send(`**${message.author.tag}** muted **${member.user.tag
 
 let zamann = zaman.replace('w', ' week').replace('d', ' day').replace('s', ' second').replace('m', ' minute').replace('h', ' hour');
 if(zamann.includes('second') && zamann.split(' ')[0] == 1) zamann = 'now'
-if(zamann.includes('second') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' seconds';
-if(zamann.includes('minute') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' minutes';
-if(zamann.includes('hour') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' hours';
-if(zamann.includes('day') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' days';
-if(zamann.includes('week') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' weeks';
-if(ms(zaman) >= 2147483647) return message.channel.send('You can mute a maximum of one for 24 days.');// ellemeyin arkadaslar.
+if(zamann.includes('second') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' saniye';
+if(zamann.includes('minute') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' dakika';
+if(zamann.includes('hour') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' saat';
+if(zamann.includes('day') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' gün';
+if(zamann.includes('week') && zamann.split(' ')[0] > 1) zamann = zamann.split(' ')[0]+' hafta';
+if(ms(zaman) >= 1000000000) return message.channel.send(new Discord.MessageEmbed().setDescription('**En fazla 2 hafta susturma atabilirsin.**'))
 
 member.roles.add(mutedROL).then(() => {
 message.channel.send(new Discord.MessageEmbed().setDescription(`**${member.user.tag}** adlı kullanıcıyı ${zamann} boyunca susturdu.  Sebep : ${reason}`))
