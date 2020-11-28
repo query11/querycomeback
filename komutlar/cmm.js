1c54cd3d53d24ac0e497659294fbb35f8edf3b6b
Glitch (atabotlist) <none>
2020-11-28 23:00:45 +0000

    💺💸 Checkpoint ./komutlar/cmm.js:23205691/2955

--- komutlar/cmm.js
+++ komutlar/cmm.js
@@ -2,7 +2,29 @@ const Discord = require('discord.js');
 
 exports.run = async(client, message) => {
   var espri = Math.floor(Math.random() * 100)
-  message.channel.send(``)
+  let e = new Discord.MessageEmbed()
+  .setDescription(`**●▬▬▬▬▬▬▬▬▬▬▬▬▬ SUNUCU REKLAMI ÖRNEĞİ  ▬▬▬▬▬▬▬▬▬▬▬▬▬●** 
+  \`\`\`
+  ●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ۵ Jau Bot List ۵ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+    🎁・Sunucumuza gelerek botunu hızlıca büyütebilirsin.
+    🎁・7/24 Aktif ekibimiz ile botlarınız hızlıca onaylanır. 
+    🎁・Sunucumuzda kod paylaşımları da yapılmaktadır.
+    🎁・Discord Linkimiz : discord.gg/BURAYADİSCORDDAVETLİNK 
+  ●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ۵ Jau Bot List ۵ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+  \`\`\`
+
+Bot Reklam Örneği ;
+
+**Jau Bot **
+
+**- 7/24 Aktif **
+
+**-Moderasyon Guard Eğlence Müzik Otorol gibi komutların bulunduğu bir bottur.**
+
+**- Sunucunuzdaki tüm işlemleri tek bir bottan halledebilmek için Jau Bot'u ekleyin.**
+
+**-Davet Linki :  [LİNK]**`)
+  message.channel.send(e)
     
  
 };
